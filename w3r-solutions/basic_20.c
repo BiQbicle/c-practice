a48f05dbea1571786699eb2fd220e0642c6a0bdb
BiQbicle <blenderplanet65@gmail.com>
2023-06-08 00:40:40 +0500

    Learned To Write The Quadratic Formula in c  And More

--- w3r-solutions/basic_20.c
+++ w3r-solutions/basic_20.c
@@ -22,8 +22,8 @@ int main() {
         get_number_from_stdin("Input c: ", "Please Input A Valid Number: ");
     double square_root = sqrt(b * b - (4 * a * c));
 
-    double root1st = (-b + square_root) / (2 * a);
-    double root2nd = (-b - square_root) / (2 * a);
+    double root1 = (-b + square_root) / (2 * a);
+    double root2 = (-b - square_root) / (2 * a);
 
     printf("Root1: ");
     printf("%lf\n", root1);
